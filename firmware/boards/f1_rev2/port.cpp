ac30d16b269ef7ad90281532c06471b516463844
Andrey Gusakov <dron0gus@gmail.com>
2022-05-30 00:59:07 +0300

    f1_rev2: TS signature

--- firmware/boards/f1_rev2/port.cpp
+++ firmware/boards/f1_rev2/port.cpp
@@ -90,8 +90,8 @@ void SaveConfiguration() {
 
 }
 
-#define TS_SIGNATURE "rusEFI Wideband Rev2"
+#define TS_SIGNATURE "rusEFI 2022.05.29.wideband_rev2"
 
 const char *getTsSignature() {
-    return BOARD_NAME; //TS_SIGNATURE;
+    return TS_SIGNATURE;
 }
