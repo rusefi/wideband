b43d80a2ac7da471859d5084510342cd485a39d6
Andrey Gusakov <dron0gus@gmail.com>
2022-07-01 22:00:15 +0300

    f1_rev2: set O0 optimization level due to issue with ADC on GD32

--- firmware/boards/f1_rev2/build_wideband.sh
+++ firmware/boards/f1_rev2/build_wideband.sh
@@ -6,6 +6,8 @@ set -e
 cd ../..
 
 export EXTRA_PARAMS="-DECHO_UART=TRUE"
+# set optimization level to 0 until ADC issue is fixed for GD32
+export USE_OPT="-O0 -ggdb -fomit-frame-pointer -falign-functions=16 -fsingle-precision-constant"
 
 # build app firmware!
 make -j12 BOARD=f1_rev2
