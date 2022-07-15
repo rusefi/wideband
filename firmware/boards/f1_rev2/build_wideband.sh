88d0f8c45da38396aa7ae1a1582b957f86adeebc
Andrey Gusakov <dron0gus@gmail.com>
2022-07-15 18:07:14 +0300

    f1_rev2: disable openblt

--- firmware/boards/f1_rev2/build_wideband.sh
+++ firmware/boards/f1_rev2/build_wideband.sh
@@ -2,6 +2,5 @@
 
 # set optimization level to 0 until ADC issue is fixed for GD32
 BOARD=f1_rev2 \
-USE_OPENBLT=yes \
 USE_OPT="-O0 -ggdb -fomit-frame-pointer -falign-functions=16 -fsingle-precision-constant" \
 	../build_f1_board.sh
