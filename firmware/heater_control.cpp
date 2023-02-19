3b8f61030ac172ed63f0c0023690e68df502174b
Andrey Gusakov <dron0gus@gmail.com>
2023-02-19 12:31:53 +0300

    heater_control: report SensorNoHeatSupply if no supply

--- firmware/heater_control.cpp
+++ firmware/heater_control.cpp
@@ -64,6 +64,8 @@ HeaterState HeaterControllerBase::GetNextState(HeaterState currentState, HeaterA
         if (heaterSupplyVoltage < HEATER_BATTETY_OFF_VOLTAGE)
         {
             m_batteryStableTimer.reset();
+            // set fault
+            SetFault(ch, Fault::SensorNoHeatSupply);
             return HeaterState::NoHeaterSupply;
         }
         else if (heaterSupplyVoltage > HEATER_BATTERY_ON_VOLTAGE)
