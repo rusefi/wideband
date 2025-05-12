cece5220be060591cd14d3f23c6a344892e6d5b5
Andrey Gusakov <dron0gus@gmail.com>
2025-05-12 18:18:36 +0300

    Config: rename RusEfiOffset to RusEfiIdx
    Keep all Idx to CAN ID math in single place.
    Each Idx occupie two consecutive CAN IDs.

--- firmware/boards/port.h
+++ firmware/boards/port.h
@@ -88,7 +88,7 @@ public:
             // enable RusEFI protocol
             afr[i].RusEfiTx = true;
             afr[i].RusEfiTxDiag = true;
-            afr[i].RusEfiIdOffset = i;
+            afr[i].RusEfiIdx = i;
 
             // Disable AemNet
             afr[i].AemNetTx = false;
@@ -99,7 +99,7 @@ public:
             // disable RusEFI protocol - not implemented
             egt[i].RusEfiTx = false;
             egt[i].RusEfiTxDiag = false;
-            egt[i].RusEfiIdOffset = i;
+            egt[i].RusEfiIdx = i;
 
             // Enable AemNet
             egt[i].AemNetTx = true;
@@ -127,7 +127,7 @@ public:
                 bool RusEfiTxDiag:1;
                 bool AemNetTx:1;
 
-                uint8_t RusEfiIdOffset;
+                uint8_t RusEfiIdx;
                 uint8_t AemNetIdOffset;
                 uint8_t pad[5];
             } afr[2];
@@ -138,7 +138,7 @@ public:
                 bool RusEfiTxDiag:1;
                 bool AemNetTx:1;
 
-                uint8_t RusEfiIdOffset;
+                uint8_t RusEfiIdx;
                 uint8_t AemNetIdOffset;
                 uint8_t pad[5];
             } egt[2];
