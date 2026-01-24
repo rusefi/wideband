2094ec327db0147678bf531a8fb614fd36daa399
FDSoftware <diego+gh@churrosoft.ar>
2026-01-24 19:33:50 -0300

    gcov coverage report

---
+++ test/ci_gcov.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# this script is used by github actions
+#
+
+rm -rf gcov_working_area
+
+mkdir gcov_working_area
+cd gcov_working_area
+mkdir gcov
+
+echo -e "\nGenerating rusEFI wideband unit test coverage"
+
+# for debug use --html-details --html-single-page --verbose  to generate a single html
+gcovr --exclude-throw-branches --exclude-unreachable-branches --decisions --merge-mode-functions=separate \
+      --exclude '/.*/googletest/' \
+      -j4  -r ../.. --html-nested gcov/index.html
